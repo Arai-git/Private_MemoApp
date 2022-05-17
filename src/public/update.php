5894ab472761eb79b21718b4b77192631c0a3cce
Arai-git <10l.mah.s11@gmail.com>
2022-05-17 09:23:14 +0900

    DDDとは関係のないコードの修正

--- src/public/update.php
+++ src/public/update.php
@@ -8,14 +8,16 @@ $pdo = new PDO(
 );
 
 session_start();
-if ($_SERVER["REQUEST_METHOD"] == "POST") $_SESSION["errors"][] = "POST送信になっていません！";
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $_SESSION['errors'][] = 'POST送信になっていません！';
+}
 
-$id = filter_input(INPUT_GET, "id");
-$title = filter_input(INPUT_POST, "title");
-$content = filter_input(INPUT_POST, "content");
-    if (empty($title) || empty($content)) $_SESSION["errors"][] = "タイトルまたは本文が記入されていません！";
-
-
+$id = filter_input(INPUT_GET, 'id');
+$title = filter_input(INPUT_POST, 'title');
+$content = filter_input(INPUT_POST, 'content');
+if (empty($title) || empty($content)) {
+    $_SESSION['errors'][] = 'タイトルまたは本文が記入されていません！';
+}
 
 $sql = 'UPDATE pages SET title = :title, content = :content WHERE id = :id';
 $statement = $pdo->prepare($sql);
@@ -24,8 +26,8 @@ $statement->bindValue(':title', $title, PDO::PARAM_STR);
 $statement->bindValue(':content', $content, PDO::PARAM_STR);
 $statement->execute();
 
-if (!empty($_SESSION["errors"])) {
-	header('Location: index.php');
-	exit;
+if (!empty($_SESSION['errors'])) {
+    header('Location: index.php');
+    exit();
 }
 ?>
